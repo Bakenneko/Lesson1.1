eac14e66b58f4f9beca207393f501720ce21605d
Andrew Kurylyk <andronniy7@gmail.com>
2023-12-17 14:38:54 +0200

    one

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {IUser} from "../interfaces";
-import {urls} from "../components";
+import {urls} from "../constants";
 
 @Injectable({
   providedIn: 'root'
